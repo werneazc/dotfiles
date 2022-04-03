dad8cf051f7b3de5bb5d78edd7ced43586b811d2
André Werner <andre.werner@b-tu.de>
2022-04-03 14:24:22 +0200

    install.sh: Add coc.settings.json to nvim installation
    Signed-off-by: André Werner <andre.werner@b-tu.de>

--- install.sh
+++ install.sh
@@ -14,17 +14,19 @@ elif [ $(uname) == "Darwin" ] ;then
     OS_NAME="mac"
 else
     echo "Error: Cannot determine OS."
-    return 1
+    exit 1
 fi
 
 if [[ $OS_NAME == "mac" ]] ; then
     install -m 755 "./mac/zshrc" "$HOME/.zshrc"
     install -m 755 "./mac/profile" "$HOME/.zprofile"
+
     install -m 755 "./git/gitconfig" "$HOME/.gitconfig"
     install -m 755 "./git/gitignore_global" "$HOME/.gitignore_global"
 
     install -m 755 -d "$HOME/.config/nvim"
-    install -m 755 "./mac/nvim/init.vim" "$HOME/.config/nvim/init.vim"
+    install -m 755 "./nvim/init.vim" "$HOME/.config/nvim/init.vim"
+    install -m 755 "./nvim/coc-settings.json" "$HOME/.config/nvim/coc-settings.json"
 fi
 
-return 0
+exit 0
